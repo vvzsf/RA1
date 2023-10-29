5879caeca4d03ca473f2febd20ea8cfc7d42595f
Arisu0000 <130484247+Arisu0000@users.noreply.github.com>
2023-10-29 16:13:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ async def start_handler(c, m):
         InlineKeyboardButton('◤ ᴜᴘᴅᴀᴛᴇꜱ ◢', url='https://t.me/PanindiaFilmZ'),
         InlineKeyboardButton('◤ ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ ◢', url='https://t.me/BoTzUpdates0')
     ]]
-    return await m.reply_text(text=START_TEXT,ACCEPTED_TEXT.format(m.from_user.mention), disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(button))
+    return await m.reply_text(text=START_TEXT.format(m.from_user.mention), disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(button))
           
 
 @Bot.on_message(filters.command(["broadcast", "users"]) & filters.user(ADMINS))  
